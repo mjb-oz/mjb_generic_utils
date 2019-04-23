1ec9fbd78da50983f1be72618b3d0567c0fc616c
mjb-oz <29187693+mjb-oz@users.noreply.github.com>
2019-04-23 16:50:52 +1000

    Add list tables and list columns functions

--- generic_utils/db_utils.py
+++ generic_utils/db_utils.py
@@ -56,3 +56,67 @@ def closeCon(con, db_path):
     os.remove(tmp_db_path)
     print('Connection to {} is closed. Temporary working copy removed.'.format(db_path))
     return
+    
+def listTables(con, all = True):
+    '''
+    Simple wrapper to list all the tables in a given database.
+    This has been setup to ignore the background tables in Spatialite.
+    
+    :param: con, a connection to a spatialite database
+    :param: all, a flag to allow the user to get all the tables, or just the non-spatialite specific ones
+    
+    :return:
+    A list of all the table names
+    '''
+    tables = [table[0] for table in con.cursor().execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()]
+    exclude_list = ['spatial_ref_sys',
+                     'spatialite_history',
+                     'sqlite_sequence',
+                     'geometry_columns',
+                     'spatial_ref_sys_aux',
+                     'views_geometry_columns',
+                     'virts_geometry_columns',
+                     'geometry_columns_statistics',
+                     'views_geometry_columns_statistics',
+                     'virts_geometry_columns_statistics',
+                     'geometry_columns_field_infos',
+                     'views_geometry_columns_field_infos',
+                     'virts_geometry_columns_field_infos',
+                     'geometry_columns_time',
+                     'geometry_columns_auth',
+                     'views_geometry_columns_auth',
+                     'virts_geometry_columns_auth',
+                     'sql_statements_log',
+                     'SpatialIndex',
+                     'ElementaryGeometries']
+    if not all:
+        tables = [table for table in tables if table not in exlude_list]
+    return tables
+
+def listColumns(table_name, con):
+    '''
+    Returns a python list containing all the columns within a single spatialite table
+    
+    :param: table_name, the name of the table of interest
+    :param: con, the connection to the spatialite database
+    
+    :return:
+    A list of all the columns in the requested table
+    '''
+    cols = [row[1] for row in con.cursor().execute("pragma table_info('{}')".format(table_name).fetchall()]
+    return cols
+    
+def describeTable(table_name, con):
+    '''
+    A function to extract table info from a spatialite database
+    
+    :param: table_name, the name of the table of interest
+    :param: con, the connection to the spatialite database
+    
+    :return:
+    A pandas DataFrame containing the details about each column
+    '''
+    desc = pd.DataFrame(con.cursor().execute("pragma table_info('{}')".format(table_name).fetchall()))
+    desc.columns = ['column_num', 'column_name', 'is_nullable','default_value','is_primary_key']
+    desc = desc.set_index('column_num', drop = True)
+    return desc
